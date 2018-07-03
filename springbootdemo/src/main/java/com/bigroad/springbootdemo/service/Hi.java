c2fc64bd410f6f63463665630f988c6856f6eee4
liuyangyang <liuyangyang@banmatrip.com>
2018-07-03 20:28:28 +0800

    dev

---
+++ springbootdemo/src/main/java/com/bigroad/springbootdemo/service/Hi.java
@@ -0,0 +1,11 @@
+package com.bigroad.springbootdemo.service.impl;
+
+/**
+ * 描述：
+ *
+ * @author liuyangyang
+ * @create 2018-07-03 16:25
+ **/
+public class Hi
+{
+}
---
+++ springbootdemo/src/main/java/com/bigroad/springbootdemo/service/imple/HiImpl.java
@@ -0,0 +1,11 @@
+package com.bigroad.springbootdemo.service.impl.service1;
+
+/**
+ * 描述：
+ *
+ * @author liuyangyang
+ * @create 2018-07-03 16:26
+ **/
+public class HiImpl
+{
+}
